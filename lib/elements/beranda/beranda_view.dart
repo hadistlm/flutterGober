de39ce309bf4ba4ddfda5f7faf13b2a337b525fe
Islaa <hadist.lm@gmail.com>
2019-09-06 13:30:02 +0700

    Initial Project

---
+++ lib/elements/beranda/beranda_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:gober/landing/landingpage_view.dart';
+
+class BerandaPage  extends StatefulWidget {
+  @override
+  _BerandaPageState createState() => new _BerandaPageState();
+}
+
+class _BerandaPageState extends State<BerandaPage> {
+	@override
+	Widget build(BuildContext context) {
+ 	   return Scaffold();
+  	}
+}
